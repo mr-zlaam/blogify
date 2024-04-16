e9148e73bc32a5725959f646a525f8149dae8901
Zlaam <mrzalaam@gmail.com>
2024-04-16 22:38:55 +0500

    build(deps): update package-lock.json, package.json and admin.controller.ts

--- src/features/admin/admin.controller.ts
+++ src/features/admin/admin.controller.ts
@@ -1,7 +1,8 @@
 import { type NextFunction, type Request, type Response } from "express";
 import { HandleError } from "../../config/HandleError";
 import type { AdminTypes } from "./admin.types";
-
+import bcrypt from "bcrypt";
+import { AdminModel } from "./admin.model";
 export default async function createAdmin(
   req: Request,
   res: Response,
@@ -26,11 +27,27 @@ export default async function createAdmin(
         }),
       );
     }
-
+    const existingAdmin = await AdminModel.find({ role: "admin" });
+    if (existingAdmin) {
+      return next(
+        res.status(403).json({
+          success: false,
+          status: 403,
+          message: "Admin already exists!",
+        }),
+      );
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newAdmin = await AdminModel.create({
+      username,
+      password: hashedPassword,
+      email,
+      role,
+    });
     return res.status(200).json({
       success: true,
       status: 200,
-      message: "Admin created successfully",
+      message: "Admin registered successfully",
     });
   } catch (error) {
     return next(HandleError(500, "unable to register admin for some reason!"));
