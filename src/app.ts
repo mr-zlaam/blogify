bf0098e148f3bd325e08adab01c861ceaad873a7
Zlaam <mrzalaam@gmail.com>
2024-04-16 13:38:28 +0500

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
 import express from "express";
 import ThrowError from "./middleware/Errorhandler.middleware";
-import error from "http-errors";
 
 export const app = express();
 app.use(express.json());
 
 app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    success: true,
-    status: 200,
-    message: "OK",
-  });
+    return res.status(200).json({
+        success: true,
+        status: 200,
+        message: "OK",
+    });
 });
 app.use(ThrowError);
